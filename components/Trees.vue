4f8c11e1a50c15a201a59278d8be4b2b6b3ff1df
scottmries <scottmries@gmail.com>
2017-07-14 15:46:01 -0400

    Hello World.

---
+++ components/Trees.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="VueToNuxtLogo">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'hello, we will be trees'
+      }
+    }
+  }
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <Trees></Trees>
+  </section>
+</template>
+
+<script>
+import Trees from '~components/Trees.vue'
+
+export default {
+  components: {
+    Trees
+  }
+}
+</script>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
